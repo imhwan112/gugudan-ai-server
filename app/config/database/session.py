a69e04af5dae118521101a49e0a7d0d578285217
나큰솔 <knsol1992@e-hcg.com>
2025-12-22 11:33:51 +0900

    refactor: [나큰솔] db pool_size, timeout 등 조정
        pool_size = 10,
        max_overflow = 20,
        pool_timeout = 30,
        pool_recycle = 1800,

--- app/config/database/session.py
+++ app/config/database/session.py
@@ -17,7 +17,11 @@ DATABASE_URL = (
 engine = create_engine(
     DATABASE_URL,
     echo=True,
-    pool_pre_ping=True
+    pool_pre_ping=True,
+    pool_size = 10,
+    max_overflow = 20,
+    pool_timeout = 30,
+    pool_recycle = 1800,
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
